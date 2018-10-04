83f2e0adc74ab08cdca86e60584ee885c02d6e77
Jacob Bennett <kf7ags@gmail.com>
2018-10-04 10:39:22 -0600

    removed calendar and added flexfrog proof

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunc(){
+    document.getElementById("theImage").setAttribute("src", "./images/Flexbox-Finish.PNG");
+}
+function revertIMG(){
+    document.getElementById("theImage").setAttribute("src", "./images/burger-hero.jpg");
+}
